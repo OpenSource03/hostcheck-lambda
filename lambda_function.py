c281c131223c9a8ea225dcefe41960aa8d0b39dd
OpenSource <opensource@thearcadia.xyz>
2022-12-04 16:33:48 +0100

    Fixed crash when server is offline

--- lambda_function.py
+++ lambda_function.py
@@ -2,17 +2,23 @@ import json
 from package.tcppinglib.tcp_ping import tcpping
 
 def lambda_handler(event, context):
-  hostname = event['queryStringParameters']['host']
+  try:
+    hostname = event['queryStringParameters']['host']
 
-  host = tcpping(hostname, 80, 1, 2, 0.2)
+    host = tcpping(hostname, 80, 1, 2, 0.2)
 
-  return {
-    'statusCode': 200,
-    'body': json.dumps({
-      'success': True,
-      'data': {
-        'latency': host.avg_rtt,
-        'host': event['headers']['Host']
-      }
-    })
-  }
+    return {
+      'statusCode': 200,
+      'body': json.dumps({
+        'success': True,
+        'data': {
+          'latency': host.avg_rtt,
+          'host': event['headers']['Host']
+        }
+      })
+    }
+  except:
+    return {
+      'statusCode': 400,
+      'body': json.dumps('Invalid or offline host')
+    }
